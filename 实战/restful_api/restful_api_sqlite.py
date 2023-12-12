000347cc99b9c4f8d1e3ceedf01c89ddd110f2d5
guoguogewangzi <879086359@qq.com>
2023-12-12 10:06:01 +0800

    update restful_api_sqlite.py

--- 实战/restful_api/restful_api_sqlite.py
+++ 实战/restful_api/restful_api_sqlite.py
@@ -67,3 +67,81 @@ def delete_user(user_id):
 
 if __name__ == '__main__':
     app.run()
+    
+    
+    
+
+
+
+'''
+1.‘查询’数据的数据包
+----------------------
+GET /users HTTP/1.1
+Host: 127.0.0.1:5000
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
+Accept-Encoding: gzip, deflate, br
+Connection: close
+----------------------
+'''
+
+'''
+2.‘查询单个用户’数据的数据包
+----------------------
+GET /users/1 HTTP/1.1
+Host: 127.0.0.1:5000
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
+Accept-Encoding: gzip, deflate, br
+Connection: close
+Upgrade-Insecure-Requests: 1
+----------------------
+
+'''
+
+'''
+3.‘插入’数据的数据包
+----------------------
+POST /users HTTP/1.1
+Host: 127.0.0.1:5000
+User-Agent: python-requests/2.25.1
+Accept-Encoding: gzip, deflate, br
+Accept: */*
+Connection: close
+Content-Length: 31
+Content-Type: application/json
+
+{"name": "John Doe", "age": 25}
+----------------------
+'''
+
+'''
+4.‘更新’数据的数据包
+----------------------
+PUT /users/1 HTTP/1.1
+Host: 127.0.0.1:5000
+User-Agent: python-requests/2.25.1
+Accept-Encoding: gzip, deflate, br
+Accept: */*
+Connection: close
+Content-Length: 31
+Content-Type: application/json
+
+{"name": "John Doe", "age": 25}
+----------------------
+'''
+
+'''
+5.‘删除’数据的数据包
+----------------------
+DELETE /users/6	 HTTP/1.1
+Host: 127.0.0.1:5000
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
+Accept-Encoding: gzip, deflate, br
+Connection: close
+----------------------
+'''
